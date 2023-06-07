806e274be23bcbd4dad99f1c6e74f766e22ee5a6
Dev43 <patrick@bitbackapp.com>
2023-06-07 12:52:33 +0200

    fixes downloads

--- frontend/src/components/chat/w2wChat/TypeBar/Files/Files.tsx
+++ frontend/src/components/chat/w2wChat/TypeBar/Files/Files.tsx
@@ -29,7 +29,7 @@ const Files = (props: FileProps) => {
       modifiedName = name;
     }
     const content = fileContent.content as string;
-    let url = URL.createObjectURL(new Blob([content]));
+    let url = URL.createObjectURL(b64toBlob(content, fileContent.type, 512));
 
     const size = fileContent.size;
 
@@ -121,3 +121,23 @@ const FileDownloadIcon = styled.i`
 `;
 
 export default Files;
+
+const b64toBlob = (b64Data, contentType = '', sliceSize = 512) => {
+  const byteCharacters = atob(b64Data);
+  const byteArrays = [];
+
+  for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+    const slice = byteCharacters.slice(offset, offset + sliceSize);
+
+    const byteNumbers = new Array(slice.length);
+    for (let i = 0; i < slice.length; i++) {
+      byteNumbers[i] = slice.charCodeAt(i);
+    }
+
+    const byteArray = new Uint8Array(byteNumbers);
+    byteArrays.push(byteArray);
+  }
+
+  const blob = new Blob(byteArrays, { type: contentType });
+  return blob;
+};
