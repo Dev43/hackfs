8c2d2093a6ffb1a11629a00bf3cc9a7380e39097
Dev43 <patrick@bitbackapp.com>
2023-06-13 09:36:01 +0200

    adds image decoding

--- frontend/src/components/chat/w2wChat/chats/Chats.tsx
+++ frontend/src/components/chat/w2wChat/chats/Chats.tsx
@@ -249,10 +249,16 @@ export default function Chats({ msg, caip10, messageBeingSent, ApproveIntent, is
                 left={isGroup ? '8px' : '34px'}
               >
                 <ImageMessage
-                  src={(JSON.parse(msg.messageContent) as FileMessageContent).content}
+                  src={URL.createObjectURL(
+                    b64toBlob((JSON.parse(msg.messageContent) as FileMessageContent).content, 'png', 512)
+                  )}
                   onClick={() => {
                     setShowImageModal(true);
-                    setImageUrl((JSON.parse(msg.messageContent) as FileMessageContent).content);
+                    setImageUrl(
+                      URL.createObjectURL(
+                        b64toBlob((JSON.parse(msg.messageContent) as FileMessageContent).content, 'png', 512)
+                      )
+                    );
                   }}
                 />
               </ReceivedMessage>
@@ -486,3 +492,23 @@ const SenderMessage = styled.div`
     max-width: 75vw;
   }
 `;
+
+const b64toBlob = (b64Data, contentType = '', sliceSize = 512) => {
+  const byteCharacters = atob(b64Data);
+  const byteArrays = [];
+
+  for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+    const slice = byteCharacters.slice(offset, offset + sliceSize);
+
+    const byteNumbers = new Array(slice.length);
+    for (let i = 0; i < slice.length; i++) {
+      byteNumbers[i] = slice.charCodeAt(i);
+    }
+
+    const byteArray = new Uint8Array(byteNumbers);
+    byteArrays.push(byteArray);
+  }
+
+  const blob = new Blob(byteArrays, { type: contentType });
+  return blob;
+};
